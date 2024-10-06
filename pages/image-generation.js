4975708d3792fde6b7e0d8ff9e3e743da6b56886
Xinyumi <miaoxinyu_resume@163.com>
2024-10-06 18:08:17 +0800

    Create image-generation.js

---
+++ pages/image-generation.js
@@ -0,0 +1,9 @@
+import ImageGeneration from '../components/ImageGeneration';
+
+export default function ImageGenerationPage() {
+  return (
+    <div>
+      <ImageGeneration />
+    </div>
+  );
+}
